c46feb8f12280c4a1f73512672c4b47236bc4b69
Richard Garrick <richg341@gmail.com>
2017-06-20 18:23:18 -0700

    add additional passing spec

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
