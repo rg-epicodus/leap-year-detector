bfe792813fcd6139bd7a95936f4dd8c36fc08c98
Richard Garrick <richg341@gmail.com>
2017-06-20 17:47:09 -0700

    add initial index with passing spec on one simple behaviour

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Business Logic Starts Here
+
+
+var leapYear = function(year) {
+  return false;
+};
+
+
+// User Interface Logic Starts Here
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
